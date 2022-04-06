7ec82e728e8e015e4894d86525770b41ad699893
tdenis29 <tdenis29@outlook.com>
2022-04-06 14:02:11 -0700

    fixings

--- easy-bank-landing/src/components/Articles.js
+++ easy-bank-landing/src/components/Articles.js
@@ -8,7 +8,7 @@ import confetti from '../assets/image-confetti.jpg'
 export default function Articles () {
     return (
         <div className="articles-section-container">
-            <div>
+            <div className='section-title-container'>
                 <h3 className="articles-section-title">Latest Articles</h3>
             </div>
 
@@ -21,7 +21,7 @@ export default function Articles () {
 
                     <div className="article-body">
                         <p className="article-author">By Wilson Hutton</p>
-                        <h4 className="article-title">Recieve money in any currency with no fees.</h4>
+                        <p className="article-title">Recieve money in any currency with no fees.</p>
                         <p className="article-blurb">The world is getting smaller and we're becoming more mobile. So why should you be forced to recieve money in only one kind of currency ...</p>
                     </div>
                 </div>
@@ -34,7 +34,7 @@ export default function Articles () {
 
                     <div className="article-body">
                         <p className="article-author">By Wilson Hutton</p>
-                        <h4 className="article-title">Treat yourself without worrying about money.</h4>
+                        <p className="article-title">Treat yourself without worrying about money.</p>
                         <p className="article-blurb">Our simple budgeting feature allows you to seperate out your spending and allows for realistic limits each month ...</p>
                     </div>
                 </div>
@@ -47,7 +47,7 @@ export default function Articles () {
 
                     <div className="article-body">
                         <p className="article-author">By Wilson Hutton</p>
-                        <h4 className="article-title">Take your Easybank Card wherever you go!</h4>
+                        <p className="article-title">Take your Easybank Card wherever you go!</p>
                         <p className="article-blurb">we want you to enjoy your travels. That's why we won't charge any fees while your abroad. We'll even show you ...</p>
                     </div>
                 </div>
@@ -60,7 +60,7 @@ export default function Articles () {
 
                     <div className="article-body">
                         <p className="article-author">By Claire Robinson</p>
-                        <h4 className="article-title">Our invite only Beta Accounts are now live!</h4>
+                        <p className="article-title">Our invite only Beta Accounts are now live!</p>
                         <p className="article-blurb">After alot of hard work by our team, we are excited to launch our closed beta. It's easy to request an invite through our site ...</p>
                     </div>
                 </div>
--- easy-bank-landing/src/components/Landing.js
+++ easy-bank-landing/src/components/Landing.js
@@ -12,7 +12,7 @@ export default function Landing () {
                 <img className="landing-img" alt="" src={hero}/>
             </div>
             <div className="landing-text-container">
-                <h1 className="landing-headline">Next generation Digital Thinking</h1>
+                <h1 className="landing-headline">Next generation digital banking</h1>
                 <p className="landing-support">Take your financial life online. Your EasyBank account will be a one-stop shop for spending, saving, budgeting, investing and much more.</p>
                 <div className='landing-button-container'>
                 <button className="landing-button">Request Invite</button>    
